b2ddff21ac199d9934d8e7273be8319a18ea990d
Kellen Presta <kpresta04@gmail.com>
2020-05-03 14:54:42 -0500

    cards displaying

--- src/components/BlackJackPage/BlackjackPage.component.js
+++ src/components/BlackJackPage/BlackjackPage.component.js
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import createDeck from "./scripts/createDeck";
+import PlayingCard from "../PlayingCard/PlayingCard.component";
+
+const deck = createDeck();
 
 export default function BlackjackPage() {
-	return <div></div>;
+	const [playerCardsState, playerCardsSet] = useState([]);
+
+	const playerCards = [];
+	deck.deal(2, [playerCards]);
+
+	console.log(playerCards);
+
+	return <div>{<PlayingCard shortString={playerCards[0].shortString} />}</div>;
 }
