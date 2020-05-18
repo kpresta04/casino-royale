4ae988eaf6a19e841e2f21cc18a2a984c39160be
Kellen Presta <kpresta04@gmail.com>
2020-05-18 10:40:17 -0500

    testing

--- functions/index.js
+++ functions/index.js
@@ -11,12 +11,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const Wallet = require("./db/models/wallet");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 //initialize firebase inorder to access its services
 admin.initializeApp(functions.config().firebase);
 
 //initialize express server
-const db = require("./db"); // loads our connection to the mongo database
 
 const app = express();
 const main = express();
@@ -26,10 +26,32 @@ main.use("/api/v1", app);
 main.use(bodyParser.json());
 main.use(bodyParser.urlencoded({ extended: false }));
 main.use(cors({ origin: true }));
+
+const mongoPW = require("./db/config");
+mongoose.Promise = global.Promise;
+const mongoDB = `mongodb+srv://dbUser1:${mongoPW}@cluster0-l36fj.mongodb.net/test?retryWrites=true&w=majority`;
+
+// mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Get the default connection
+// const db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
+// db.once("open", () => {
+// 	console.log(
+// 		`You have successfully connected to your mongo database: ${mongoDB}`
+// 	);
+// });
+
 main.get("/hello", (req, res) => res.send("hello dawg"));
 
 main.get("/chips/:userID", async (req, res) => {
 	try {
+		await mongoose.connect(mongoDB, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
 		const results = await Wallet.findOne({ userID: req.params.userID });
 		res.send(results);
 	} catch (error) {
