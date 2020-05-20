c86be2f77d3cb7f7bca78cd403947ba49d5783a3
Kellen Presta <kpresta04@gmail.com>
2020-05-20 14:52:45 -0500

    reducer fixed

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -5,10 +5,6 @@ const userReducer = (state = {}, action) => {
 			console.log(state);
 			return state;
 
-		case "SET_CHIP_COUNT":
-			state = { currentUser: state.user, chips: action.count };
-			return state;
-
 		default:
 			return state;
 	}
