22f5c6476916bd4434e394a458761be75f29e593
Kellen Presta <kpresta04@gmail.com>
2020-05-22 10:40:09 -0500

    blackjack css

--- client/src/components/BlackJackPage/BlackjackPage.component.js
+++ client/src/components/BlackJackPage/BlackjackPage.component.js
@@ -27,7 +27,7 @@ const useStyles = makeStyles({
 		background: "rgb(252, 245, 21)",
 		borderRadius: 3,
 		border: 0,
-		color: "white",
+
 		height: 48,
 		padding: "0 30px",
 		boxShadow: "0 1px 5px 1px rgb(0, 0, 0)",
@@ -350,31 +350,33 @@ function BlackjackPage(props) {
 
 					<div className="doubleDownBox">
 						<h2>Current Bet: {bet}</h2>
-						<Button className="chipButton" color="inherit">
-							<img
-								src="https://firebasestorage.googleapis.com/v0/b/casino-royale-9c472.appspot.com/o/gaming.svg?alt=media&token=3058a860-e55f-4cbb-aaf9-ee94e79433ce"
-								style={{ height: "24px", width: "24px", marginRight: "1em" }}
-							/>
-							{props.chips ? props.chips : 0}
-						</Button>
-						<Button
-							classes={{
-								root: classes.hit, // class name, e.g. `classes-nesting-root-x`
-								label: classes.label, // class name, e.g. `classes-nesting-label-x`
-							}}
-							id="doubleDown-button"
-							variant="outlined"
-							color="primary"
-							disabled
-							style={{ margin: "0 1em", height: "4em", width: "7em" }}
-							// onClick={() => {
-							// 	if (playersTurn) {
-							// 		runPlayerTurn();
-							// 	}
-							// }}
-						>
-							DOUBLE DOWN
-						</Button>
+						<div className="doubleDownButtons">
+							<Button className="chipButton" color="inherit">
+								<img
+									src="https://firebasestorage.googleapis.com/v0/b/casino-royale-9c472.appspot.com/o/gaming.svg?alt=media&token=3058a860-e55f-4cbb-aaf9-ee94e79433ce"
+									style={{ height: "24px", width: "24px", marginRight: "1em" }}
+								/>
+								{props.chips ? props.chips : 0}
+							</Button>
+							<Button
+								classes={{
+									root: classes.hit, // class name, e.g. `classes-nesting-root-x`
+									label: classes.label, // class name, e.g. `classes-nesting-label-x`
+								}}
+								id="doubleDown-button"
+								variant="outlined"
+								color="primary"
+								disabled
+								style={{ margin: "0 1em", height: "4em", width: "7em" }}
+								// onClick={() => {
+								// 	if (playersTurn) {
+								// 		runPlayerTurn();
+								// 	}
+								// }}
+							>
+								DOUBLE DOWN
+							</Button>
+						</div>
 					</div>
 
 					<div className="playerCards">
@@ -396,44 +398,46 @@ function BlackjackPage(props) {
 					</div>
 					{/* <h1 id="announce-text">{announceText}</h1> */}
 					<div className="playerButtons">
-						<Button
-							classes={{
-								root: classes.hit, // class name, e.g. `classes-nesting-root-x`
-								label: classes.label, // class name, e.g. `classes-nesting-label-x`
-							}}
-							id="hit-button"
-							size="large"
-							color="primary"
-							style={{ margin: "0 1em", height: "3em", width: "7em" }}
-							onClick={() => {
-								if (playersTurn) {
-									runPlayerTurn();
-								}
-							}}
-						>
-							HIT
-						</Button>
+						<div className="player-buttons-container">
+							<Button
+								classes={{
+									root: classes.hit, // class name, e.g. `classes-nesting-root-x`
+									label: classes.label, // class name, e.g. `classes-nesting-label-x`
+								}}
+								id="hit-button"
+								size="large"
+								color="primary"
+								style={{ margin: "0 1em", height: "3em", width: "7em" }}
+								onClick={() => {
+									if (playersTurn) {
+										runPlayerTurn();
+									}
+								}}
+							>
+								HIT
+							</Button>
 
-						<Button
-							classes={{
-								root: classes.stand, // class name, e.g. `classes-nesting-root-x`
-								label: classes.label, // class name, e.g. `classes-nesting-label-x`
-							}}
-							style={{
-								margin: "0 1em",
-								height: "3em",
-								width: "7em",
-							}}
-							size="large"
-							// disabled={!running}
-							onClick={() => {
-								if (playersTurn) {
-									stand();
-								}
-							}}
-						>
-							STAND
-						</Button>
+							<Button
+								classes={{
+									root: classes.stand, // class name, e.g. `classes-nesting-root-x`
+									label: classes.label, // class name, e.g. `classes-nesting-label-x`
+								}}
+								style={{
+									margin: "0 1em",
+									height: "3em",
+									width: "7em",
+								}}
+								size="large"
+								// disabled={!running}
+								onClick={() => {
+									if (playersTurn) {
+										stand();
+									}
+								}}
+							>
+								STAND
+							</Button>
+						</div>
 					</div>
 				</div>
 			</div>
